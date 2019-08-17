b8c00e77e09f26e242cff737e246440f307d8bc5
taha <taha.azzabi@gmail.com>
2019-08-18 00:07:13 +0100

    clean up

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   output: {
     // '__dirname' is a Node variable that gives us the absolute
-    // path to our current directory. 
-    // path.join(): a method to joins directory 
+    // path to our current directory.
+    // path.join(): a method to joins directory
     path: path.resolve(__dirname, '../', 'build'),
-    //Setting where to output compiled files
+    // Setting where to output compiled files
     filename: 'static/js/[name].[hash].js',
     publicPath: '/',
   },
   plugins: [
     new Dotenv({
       path: './.env.development',
-    })
+    }),
   ],
 };
