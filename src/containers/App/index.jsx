1602613d998168d0ae298d8e2e2a1e5cbc728627
taha <taha.azzabi@gmail.com>
2019-08-18 01:50:04 +0100

    Ajouter des balises titre et description par défaut

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from '../HomePage';
@@ -6,6 +7,14 @@ import NotFoundPage from '../NotFoundPage';
 
 const App = () => (
   <div className="App">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Kit de démarrage ReactJS</title>
+      <meta
+        name="description"
+        content="Kit de démarrage ReactJS en utilisant les dernières technologie disponible et en mettant l'accent les meilleures pratiques."
+      />
+    </Helmet>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/404-not-found" component={NotFoundPage} />
