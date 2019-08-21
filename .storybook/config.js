ed8e0a4ed4d06913f62db75fe7c14ea9a217bfd2
taha <taha.azzabi@gmail.com>
2019-08-21 12:32:10 +0100

    Dynamique import stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,23 @@
-// config.js
+import { configure } from '@storybook/react';
 
-import { configure } from '@storybook/react'
+/*
+  * Find and load all files that ends with .stories.js inside the
+    src folder and their subdirectories
+  * See webpack doc to find out more on "require.context" webpack doc
+  * https://webpack.js.org/guides/dependency-management/
 
-function loadStories() {
-  require('../src/components/Heading/Heading.story')
-}
+  * /\.stories.js$/ : Regular expression
+  * \. => To escape the dot because it's a special character
+  * $  => Matcher end string
+  *
+*/
+const req = require.context('../src', true, /\.stories.js$/);
 
-configure(loadStories, module)
+/**
+ * Load all Stories
+ */
+const loadStories = () => {
+  req.keys().forEach(filename => req(filename));
+};
+
+configure(loadStories, module);
