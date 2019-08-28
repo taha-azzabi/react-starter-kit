c94665bd604de70dd6ccc5f35ac24aeb2f49da30
taha <taha.azzabi@gmail.com>
2019-08-28 15:09:32 +0100

    fix prop type in root.production

--- src/Root.production.jsx
+++ src/Root.production.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
 const Root = ({ children, initialState = {} }) => {
   const store = configureStore(initialState);
   return <Provider store={store}>{children}</Provider>;
+};
+
+Root.propTypes = {
+  initialState: PropTypes.object,
+  children: PropTypes.node.isRequired,
 };
 
 export default Root;
